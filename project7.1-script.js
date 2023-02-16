a0356bb53c958c0f129e1064fc033924ddc7a4ce
Sunitha45 <123886147+Sunitha45@users.noreply.github.com>
2023-02-16 23:56:42 +0530

    Add files via upload

---
+++ project7.1-script.js
@@ -0,0 +1,4 @@
+const ja=document.getElementById('a1');
+ja.addEventListener("click",functiion(){
+     document.write('<a target="_blank" href="https://www.ineuron.ai">JavaScript 2.0</a>');
+});
---
+++ project9.2-script.js
@@ -0,0 +1,4 @@
+const btnEl=document.querySelector('.b1');
+btnEl.addEventListener('click', ()=>{
+    btnEl.classList.add('special');
+});
